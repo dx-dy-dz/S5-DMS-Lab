8c0ba14ab57cf2ee4dad20057c08fc7c631f6f77
Divya Das <72966656+dx-dy-dz@users.noreply.github.com>
2021-12-15 20:19:26 +0530

    Update exercises.sql

--- Lab-Cycle-1/exercises.sql
+++ Lab-Cycle-1/exercises.sql
@@ -7,5 +7,12 @@ CREATE database Library;
 // d. Create and execute ALTER TABLE command in tables with data and without data.
 // To change the name of table
 ALTER table Book RENAME Books;
+
 // To change the column name
 ALTER table Language CHANGE  Language_id Language_ID int(11);
+
+// To add a new column 
+alter table Language add region varchar(20);
+
+// To drop a column
+alter table Language drop region;
