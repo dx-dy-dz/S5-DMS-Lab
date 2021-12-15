aa18fabdecab35e5ef0a2763fd8e9bbee2dd31a6
Divya Das <72966656+dx-dy-dz@users.noreply.github.com>
2021-12-15 20:27:01 +0530

    Update exercises.sql

--- Lab-Cycle-1/exercises.sql
+++ Lab-Cycle-1/exercises.sql
@@ -24,8 +24,8 @@ SHOW indexes from Book_Issue;
 
 // f. Create and execute GRANT/REVOKE commands on tables.
 create user 'myuser'@'%' identified by 'password';
-GRANT select on Book to myuser;
-GRANT select,alter on Book to myuser;
+GRANT SELECT on Book to myuser;
+GRANT SELECT,ALTER on Book to myuser;
 show grants;
-REVOKE select on Book from myuser;
-GRANT select,alter on Book to myuser;
+REVOKE SELECT on Book from myuser;
+GRANT SELECT,ALTER on Book to myuser;
